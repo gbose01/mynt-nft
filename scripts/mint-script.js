4a940be7206a5f66b4b88ebba001033a06978249
gbose01 <gaurabbose01@gmail.com>
2021-09-03 17:01:11 -0700

    added loops

--- scripts/mint-script.js
+++ scripts/mint-script.js
@@ -7,11 +7,26 @@ const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 
 async function main() {
-  const NFT = await hre.ethers.getContractFactory("MyNFT");
-  const URI = "ipfs://QmdJg65XCMjJXQJLtuZJB9hEMnXnfcsrqdmBAzSu7J4rbg";
-  const WALLET_ADDRESS = PUBLIC_KEY;
-  const contract = NFT.attach(CONTRACT_ADDRESS);
-  await contract.mint(WALLET_ADDRESS, URI);
+  for(let i = 0; i < 20; i++)
+  {
+    const NFT = await hre.ethers.getContractFactory("MyNFT");
+    if(i<10)
+    {
+      var URI = "ipfs://QmXC1CZYiZ6EgAxmvtz3kkxEQsARgtWHkxz8rsguUDW7bS";
+    }
+    else if(i<30)
+    {
+      var URI = "ipfs://Qmczo4nsjsWk1onaDsPd7E5AHFQqXQ6aKmbUWJJMP2TnG5";
+    }
+    else if(i<100)
+    {
+      var URI = "ipfs://QmQykAQEmAWBra6LKotLE2NjZrjzzsfdeBXyrf74vanr5s";
+    }
+    const WALLET_ADDRESS = PUBLIC_KEY;
+    const contract = NFT.attach(CONTRACT_ADDRESS);
+    await contract.mint(WALLET_ADDRESS, URI);
+  }
+  
 }
 main().then(() => process.exit(0)).catch(error => {
   console.error(error);
